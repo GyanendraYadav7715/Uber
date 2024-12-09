dc50c0711e2c6605596ab99c46426d610027cc1f
DESKTOP-VFB5TE0\Gyanendra Yadav <gyanendrayadav12345789@gmail.com>
2024-12-09 13:51:25 +0530

    Frontend setup done

---
+++ frontend/src/pages/CaptainSignup.jsx
@@ -0,0 +1,11 @@
+ 
+
+const CaptainSignup = () => {
+  return (
+    <div>
+      <h1 className="text-black">CaptainSignup</h1>
+    </div>
+  );
+}
+
+export default CaptainSignup
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+ 
+
+const Home = () => {
+  return (
+      <div className="h-screen w-screen bg-black">
+      <h1>Uber</h1>
+      </div>
+  )
+}
+
+export default Home
